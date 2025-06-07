75ef71af527dbe837bb34e3dc5eb2e3298c54c36
Selim Ahmed <aronno1920@hotmail.com>
2025-06-07 16:08:42 +0600

    Update ProductSeeder.cs

--- PracticeApi/Entities/ProductSeeder.cs
+++ PracticeApi/Entities/ProductSeeder.cs
@@ -5,14 +5,14 @@ namespace PracticeApi.Entities
 {
     public class ProductSeeder
     {
-        public static async Task SeedAsync(AppDbContext context, int count = 100000)
+        public static async Task SeedAsync(AppDbContext context, int count = 200000)
         {
             if (context.Products.Any()) return;
 
             var faker = new Faker<Product>()
                 .RuleFor(p => p.Name, f => f.Commerce.ProductName())
                 .RuleFor(p => p.Description, f => f.Commerce.ProductDescription())
-                .RuleFor(p => p.Price, f => f.Random.Decimal(10, 1000))
+                .RuleFor(p => p.Price, f => f.Random.Decimal(10, 10000))
                 .RuleFor(p => p.Category, f => f.Commerce.Categories(1)[0]);
 
             var products = faker.Generate(count);
