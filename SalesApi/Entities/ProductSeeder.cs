d65f9058f7f05958af2370a068deeb78f73e05c2
Selim Ahmed <aronno1920@hotmail.com>
2025-06-07 11:13:01 +0600

    Create DbContext and Seeder Class

---
+++ SalesApi/Entities/ProductSeeder.cs
@@ -0,0 +1,24 @@
+﻿using Bogus;
+using SalesApi.Data;
+using SalesCore.Entities;
+
+namespace SalesApi.Entities
+{
+    public class ProductSeeder
+    {
+        public static async Task SeedAsync(AppDbContext context, int count = 10000)
+        {
+            if (context.Products.Any()) return;
+
+            var faker = new Faker<Product>()
+                .RuleFor(p => p.Name, f => f.Commerce.ProductName())
+                .RuleFor(p => p.Description, f => f.Commerce.ProductDescription())
+                .RuleFor(p => p.Price, f => f.Random.Decimal(10, 1000))
+                .RuleFor(p => p.Category, f => f.Commerce.Categories(1)[0]);
+
+            var products = faker.Generate(count);
+            await context.Products.AddRangeAsync(products);
+            await context.SaveChangesAsync();
+        }
+    }
+}
--- SalesApi/Program.cs
+++ SalesApi/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using SalesApi.Data;
+using SalesApi.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +10,30 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+
+
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+
 var app = builder.Build();
+
+
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    context.Database.Migrate();
+    await ProductSeeder.SeedAsync(context);
+}
+
+
+
+
+
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
