30b1e45ebb185f97e038d07cbd24b4f58ccfd52c
Selim Ahmed <aronno1920@hotmail.com>
2025-06-07 12:02:06 +0600

    update

--- SalesApi/Data/AppDbContext.cs
+++ SalesApi/Data/AppDbContext.cs
@@ -5,8 +5,21 @@ namespace SalesApi.Data
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
 
         public DbSet<Product> Products => Set<Product>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.Property(e => e.Price).HasColumnType("decimal(18, 2)");
+            });
+        }
     }
 }
